f95c0373455f7a5dfebb957fd65ba82cecc07bc8
Marco Elizalde <m.elizalde.torres@gmail.com>
2020-12-30 07:04:16 -0600

    add se agrega soporte para conversion de monedas colimbiana, argetina y mexicana

--- conversor.py
+++ conversor.py
@@ -1,6 +1,26 @@
+menu = """BIENVENIDO AL CONVERSOR DE MODEDAS
+
+1 - Pesos Colombianos
+2 - Pesos Argentinos
+3 - Pesos Mexicanos
+
+Elige una opción: """
+
+opcion = int(input(menu))
+
+if opcion == 1:
+    valor_dolar = 3875
+elif opcion == 2:
+    valor_dolar = 65
+elif opcion == 3:
+    valor_dolar = 20
+else:
+    print('Ingresa una opción correcta por favor')
+    exit()
+
+
 pesos = input("¿Cuántos pesos tienes?: ")
 pesos = float(pesos)
-valor_dolar = 3875
 dolares = pesos / valor_dolar
 dolares = str(dolares)
 print("Tienes $ " + dolares + " dólares")
